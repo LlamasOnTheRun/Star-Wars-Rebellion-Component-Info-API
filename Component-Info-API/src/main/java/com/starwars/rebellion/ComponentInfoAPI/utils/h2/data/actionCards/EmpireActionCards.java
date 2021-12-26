263afc02c4c98c9ebf5f1efe9323f7445abafeac
Ryan Llamas <49294244+LlamasOnTheRun@users.noreply.github.com>
2021-12-25 21:37:42 -0600

    Added two more entitys for action cards
    Signed-off-by: Ryan Llamas <49294244+LlamasOnTheRun@users.noreply.github.com>

--- Component-Info-API/src/main/java/com/starwars/rebellion/ComponentInfoAPI/utils/h2/data/actionCards/EmpireActionCards.java
+++ Component-Info-API/src/main/java/com/starwars/rebellion/ComponentInfoAPI/utils/h2/data/actionCards/EmpireActionCards.java
@@ -13,11 +13,15 @@ import java.util.List;
 @Service
 public class EmpireActionCards {
     public static final ActionCard MORE_DANGEROUS_THAN_YOU_REALIZE = getMoreDangerousThanYouRealize();
+    public static final ActionCard BRILLIANT_ADMINISTRATOR = getBrilliantAdministrator();
+    public static final ActionCard ACCORDING_TO_MY_DESIGN = getAccordingToMyDesign();
 
     public static List<ActionCard> fetch() {
         List<ActionCard> empireActionCards = new ArrayList<>();
 
         empireActionCards.add(MORE_DANGEROUS_THAN_YOU_REALIZE);
+        empireActionCards.add(BRILLIANT_ADMINISTRATOR);
+        empireActionCards.add(ACCORDING_TO_MY_DESIGN);
 
         return empireActionCards;
     }
@@ -27,7 +31,10 @@ public class EmpireActionCards {
         ActionCardText actionCardText = new ActionCardText();
         ArrayList<Leader> leaders = new ArrayList<>();
 
-        actionCardText.setTitle("More Dangerous Than You Realize");
+        actionCardText.setTitle("""
+                More Dangerous
+                Than You Realize
+                """);
         actionCardText.setCardType("Start of Combat");
         actionCardText.setDescription("""
                 Draw either 3 space tactic
@@ -35,8 +42,59 @@ public class EmpireActionCards {
                 """);
         actionCard.setActionCardText(actionCardText);
         actionCard.setFaction(Faction.IMPERIAL);
-        actionCard.setStartingCard(true);
+        actionCard.setStartingCard(false);
         leaders.add(EmpireLeaders.GENERAL_TAGGE);
+        actionCard.setLeaderChoices(leaders);
+
+        return actionCard;
+    }
+
+    private static ActionCard getBrilliantAdministrator() {
+        ActionCard actionCard = new ActionCard();
+        ActionCardText actionCardText = new ActionCardText();
+        ArrayList<Leader> leaders = new ArrayList<>();
+
+        actionCardText.setTitle("""
+                Brilliant
+                Administrator
+                """);
+        actionCardText.setCardType("Assignment");
+        actionCardText.setDescription("""
+                Place this leader in any
+                Imperial system. Then
+                place units on the build
+                queue using this system's
+                resource icons and number.
+                """);
+        actionCard.setActionCardText(actionCardText);
+        actionCard.setFaction(Faction.IMPERIAL);
+        actionCard.setStartingCard(false);
+        leaders.add(EmpireLeaders.GRAND_MOFF_TARKIN);
+        actionCard.setLeaderChoices(leaders);
+
+        return actionCard;
+    }
+
+    private static ActionCard getAccordingToMyDesign() {
+        ActionCard actionCard = new ActionCard();
+        ActionCardText actionCardText = new ActionCardText();
+        ArrayList<Leader> leaders = new ArrayList<>();
+
+        actionCardText.setTitle("""
+                According
+                To My Design
+                """);
+        actionCardText.setCardType("Start of Combat");
+        actionCardText.setDescription("""
+                For each battle during the
+                first combat round, the Rebel
+                player rolls 1 fewer red die
+                and 2 fewer black dice.
+                """);
+        actionCard.setActionCardText(actionCardText);
+        actionCard.setFaction(Faction.IMPERIAL);
+        actionCard.setStartingCard(false);
+        leaders.add(EmpireLeaders.EMPOROR_PALPATINE);
         actionCard.setLeaderChoices(leaders);
 
         return actionCard;

5893729fec186b18091841d19ece42959c0ae810
Ryan Llamas <49294244+LlamasOnTheRun@users.noreply.github.com>
2022-02-12 14:14:59 -0600

    Added all data for region five
    Signed-off-by: Ryan Llamas <49294244+LlamasOnTheRun@users.noreply.github.com>

--- Component-Info-API/src/main/java/com/starwars/rebellion/ComponentInfoAPI/utils/h2/data/systems/RegionFiveSystemData.java
+++ Component-Info-API/src/main/java/com/starwars/rebellion/ComponentInfoAPI/utils/h2/data/systems/RegionFiveSystemData.java
@@ -6,29 +6,68 @@ import com.starwars.rebellion.ComponentInfoAPI.utils.h2.data.regions.RegionData;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.starwars.rebellion.ComponentInfoAPI.utils.APIConstants.NEUTRAL_LOYALTY;
-import static com.starwars.rebellion.ComponentInfoAPI.utils.APIConstants.SHIP_PRODUCTION_SYMBOL_FIRST;
+import static com.starwars.rebellion.ComponentInfoAPI.utils.APIConstants.*;
 
 public class RegionFiveSystemData {
     public static final System MUSTAFAR = getMustafar();
+    public static final System BESPIN = getBespin();
+    public static final System ENDOR = getEndor();
+    public static final System HOTH = getHoth();
 
     public static List<System> fetch() {
         List<System> systems = new ArrayList<>();
         systems.add(MUSTAFAR);
+        systems.add(BESPIN);
+        systems.add(ENDOR);
+        systems.add(HOTH);
         return systems;
     }
 
     private static System getMustafar() {
         System system = new System();
 
-        system.setRegion(RegionData.REGION_ONE);
-        system.setName("Mon Calamari");
+        system.setRegion(RegionData.REGION_FIVE);
+        system.setName("Mustafar");
         system.setRemote(false);
         system.setLoyalty(NEUTRAL_LOYALTY);
         system.getShipProduction().setShipLight(1);
-        system.getShipProduction().setShipHeavy(1);
+        system.getShipProduction().setShipMedium(1);
         system.setFirstProductionSymbol(SHIP_PRODUCTION_SYMBOL_FIRST);
-        system.setQueueTime(3);
+        system.setQueueTime(2);
+
+        return system;
+    }
+
+    private static System getBespin() {
+        System system = new System();
+
+        system.setRegion(RegionData.REGION_FIVE);
+        system.setName("Bespin");
+        system.setRemote(false);
+        system.setLoyalty(NEUTRAL_LOYALTY);
+        system.getLandProduction().setLandMedium(1);
+        system.setFirstProductionSymbol(LAND_PRODUCTION_SYMBOL_FIRST);
+        system.setQueueTime(1);
+
+        return system;
+    }
+
+    private static System getEndor() {
+        System system = new System();
+
+        system.setRegion(RegionData.REGION_FIVE);
+        system.setName("Endor");
+        system.setRemote(true);
+
+        return system;
+    }
+
+    private static System getHoth() {
+        System system = new System();
+
+        system.setRegion(RegionData.REGION_FIVE);
+        system.setName("Hoth");
+        system.setRemote(true);
 
         return system;
     }

635ef6ba6c652b1962bf22bd22e73eecb693c1a5
Ryan Llamas <49294244+LlamasOnTheRun@users.noreply.github.com>
2022-02-27 20:28:43 -0600

    Additional unit ground for rebel is reflection on tests
    Signed-off-by: Ryan Llamas <49294244+LlamasOnTheRun@users.noreply.github.com>

--- Component-Info-API/src/main/java/com/starwars/rebellion/ComponentInfoAPI/utils/h2/data/units/RebelGroundUnitData.java
+++ Component-Info-API/src/main/java/com/starwars/rebellion/ComponentInfoAPI/utils/h2/data/units/RebelGroundUnitData.java
@@ -9,13 +9,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static com.starwars.rebellion.ComponentInfoAPI.utils.APIConstants.BLACK_HEALTH_COLOR;
+import static com.starwars.rebellion.ComponentInfoAPI.utils.APIConstants.RED_HEALTH_COLOR;
 
 public class RebelGroundUnitData {
     public static final Unit REBEL_TROOPER = getRebelTrooper();
+    public static final Unit AIR_SPEEDER = getAirSpeeder();
 
     public static List<Unit> fetch() {
         List<Unit> systems = new ArrayList<>();
         systems.add(REBEL_TROOPER);
+        systems.add(AIR_SPEEDER);
         return systems;
     }
 
@@ -30,6 +33,21 @@ public class RebelGroundUnitData {
         unit.setHealthColor(BLACK_HEALTH_COLOR);
         unit.setNeedsTransport(true);
         unit.setTotalInGame(15);
+        return unit;
+    }
+
+    private static Unit getAirSpeeder() {
+        Unit unit = new Unit();
+        unit.setTitle("Airspeeder");
+        unit.setFaction(Faction.REBEL);
+        unit.setUnitType(UnitType.GROUND);
+        unit.setProductionType(ProductionType.MEDIUM);
+        unit.setBlackDie(1);
+        unit.setRedDie(1);
+        unit.setHealth(2);
+        unit.setHealthColor(RED_HEALTH_COLOR);
+        unit.setNeedsTransport(true);
+        unit.setTotalInGame(4);
         return unit;
     }
 }

093e267d91793388776a825602c8b7de631a89ec
alexrojas431 <alexrojas431@gmail.com>
2022-04-25 19:48:14 -0500

    Added predicate testcases in specificationTest file -more test cases to be added

--- Component-Info-API/src/test/java/com/starwars/rebellion/ComponentInfoAPI/specification/ActionCardSpecificationTest.java
+++ Component-Info-API/src/test/java/com/starwars/rebellion/ComponentInfoAPI/specification/ActionCardSpecificationTest.java
@@ -1,21 +1,53 @@
 package com.starwars.rebellion.ComponentInfoAPI.specification;
 
+import com.starwars.rebellion.ComponentInfoAPI.dao.entities.ActionCard;
+import com.starwars.rebellion.ComponentInfoAPI.dao.request.ActionCardRequest;
+import com.starwars.rebellion.ComponentInfoAPI.repositories.ActionCardRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
 public class ActionCardSpecificationTest {
 
     @Autowired
     private ActionCardSpecification actionCardSpecification;
+    @Autowired
+    private ActionCardRepository actionCardRepository;
 
     @Test
     void givenIDIsLessThanOne_thenIDEqualPredicateIsNotAdded() {
+        ActionCardRequest actionCardRequest = new ActionCardRequest();
+        actionCardRequest.setId(0);
 
+        List<ActionCard> actionCardList = actionCardRepository.findAll(actionCardSpecification.getActionCards(actionCardRequest));
+
+        assertTrue(actionCardList.size() > 1);
     }
 
     @Test
     void givenIDIsGreaterThanZero_thenIDEqualPredicateIsAdded() {
+        ActionCardRequest actionCardRequest = new ActionCardRequest();
+        actionCardRequest.setId(26);
 
+        List<ActionCard> actionCardList = actionCardRepository.findAll(actionCardSpecification.getActionCards(actionCardRequest));
+
+        assertEquals(1, actionCardList.size());
+    }
+
+    @Test
+    void givenIDDoesNotExist_thenIDEqualPredicateIsStillAdded() {
+        ActionCardRequest actionCardRequest = new ActionCardRequest();
+        actionCardRequest.setId(1);
+
+        List<ActionCard> actionCardList = actionCardRepository.findAll(actionCardSpecification.getActionCards(actionCardRequest));
+
+        assertEquals(0, actionCardList.size());
     }
 
     @Test

e51f785d3b40aa30e63f6dca745b8d0c58ebf163
Ryan Llamas <49294244+LlamasOnTheRun@users.noreply.github.com>
2022-05-18 18:35:08 -0500

    Removed unneeded method calls for repository
    Signed-off-by: Ryan Llamas <49294244+LlamasOnTheRun@users.noreply.github.com>

--- Component-Info-API/src/test/java/com/starwars/rebellion/ComponentInfoAPI/repositories/ActionCardRepositoryTest.java
+++ Component-Info-API/src/test/java/com/starwars/rebellion/ComponentInfoAPI/repositories/ActionCardRepositoryTest.java
@@ -2,11 +2,12 @@ package com.starwars.rebellion.ComponentInfoAPI.repositories;
 
 import com.starwars.rebellion.ComponentInfoAPI.dao.entities.ActionCard;
 import com.starwars.rebellion.ComponentInfoAPI.dao.entities.embeddables.Faction;
+import com.starwars.rebellion.ComponentInfoAPI.dao.request.ActionCardRequest;
+import com.starwars.rebellion.ComponentInfoAPI.specification.ActionCardSpecification;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.List;
+import org.springframework.data.jpa.domain.Specification;
 
 import static com.starwars.rebellion.ComponentInfoAPI.utils.APIConstants.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -17,38 +18,69 @@ class ActionCardRepositoryTest {
 	@Autowired
 	ActionCardRepository actionCardRepository;
 
+	@Autowired
+	ActionCardSpecification specification;
+
 	@Test
 	void givenAllRebelActionCardDataIsAvailable_thenSixNonStartingCardsAreAvailable() {
-		assertEquals(TOTAL_NON_STARTING_REBEL_ACTION_CARDS, actionCardRepository.findByIsStartingCardAndFaction(false, Faction.REBEL).size());
+		ActionCardRequest actionCardRequest = new ActionCardRequest();
+		actionCardRequest.setStartingCard(false);
+		actionCardRequest.setFaction(Faction.REBEL);
+		Specification<ActionCard> actionCardSpecification = specification.getActionCards(actionCardRequest);
+
+		assertEquals(TOTAL_NON_STARTING_REBEL_ACTION_CARDS, actionCardRepository.findAll(actionCardSpecification).size());
 	}
 
 	@Test
 	void givenAllRebelActionCardDataIsAvailable_thenTwelveStartingCardsAreAvailable() {
-		assertEquals(TOTAL_STARTING_REBEL_ACTION_CARDS, actionCardRepository.findByIsStartingCardAndFaction(true, Faction.REBEL).size());
+		ActionCardRequest actionCardRequest = new ActionCardRequest();
+		actionCardRequest.setStartingCard(true);
+		actionCardRequest.setFaction(Faction.REBEL);
+		Specification<ActionCard> actionCardSpecification = specification.getActionCards(actionCardRequest);
+
+		assertEquals(TOTAL_STARTING_REBEL_ACTION_CARDS, actionCardRepository.findAll(actionCardSpecification).size());
 	}
 
 	@Test
 	void givenAllRebelActionCardDataIsAvailable_thenEighteenCardsAreReturned() {
-		assertEquals(TOTAL_REBEL_ACTION_CARDS, actionCardRepository.findByFaction(Faction.REBEL).size());
+		ActionCardRequest actionCardRequest = new ActionCardRequest();
+		actionCardRequest.setFaction(Faction.REBEL);
+		Specification<ActionCard> actionCardSpecification = specification.getActionCards(actionCardRequest);
+
+		assertEquals(TOTAL_REBEL_ACTION_CARDS, actionCardRepository.findAll(actionCardSpecification).size());
 	}
 
 	@Test
 	void givenAllEmpireActionCardDataIsAvailable_thenFourNonStartingCardsAreAvailable() {
-		assertEquals(TOTAL_NON_STARTING_EMPIRE_ACTION_CARDS, actionCardRepository.findByIsStartingCardAndFaction(false, Faction.IMPERIAL).size());
+		ActionCardRequest actionCardRequest = new ActionCardRequest();
+		actionCardRequest.setStartingCard(false);
+		actionCardRequest.setFaction(Faction.IMPERIAL);
+		Specification<ActionCard> actionCardSpecification = specification.getActionCards(actionCardRequest);
+
+		assertEquals(TOTAL_NON_STARTING_EMPIRE_ACTION_CARDS, actionCardRepository.findAll(actionCardSpecification).size());
 	}
 
 	@Test
 	void givenAllEmpireActionCardDataIsAvailable_thenTwelveStartingCardsAreAvailable() {
-		assertEquals(TOTAL_STARTING_EMPIRE_ACTION_CARDS, actionCardRepository.findByIsStartingCardAndFaction(true, Faction.IMPERIAL).size());
+		ActionCardRequest actionCardRequest = new ActionCardRequest();
+		actionCardRequest.setStartingCard(true);
+		actionCardRequest.setFaction(Faction.IMPERIAL);
+		Specification<ActionCard> actionCardSpecification = specification.getActionCards(actionCardRequest);
+
+		assertEquals(TOTAL_STARTING_EMPIRE_ACTION_CARDS, actionCardRepository.findAll(actionCardSpecification).size());
 	}
 
 	@Test
 	void givenAllEmpireActionCardDataIsAvailable_thenSixteenCardsAreReturned() {
-		assertEquals(TOTAL_EMPIRE_ACTION_CARDS, actionCardRepository.findByFaction(Faction.IMPERIAL).size());
+		ActionCardRequest actionCardRequest = new ActionCardRequest();
+		actionCardRequest.setFaction(Faction.IMPERIAL);
+		Specification<ActionCard> actionCardSpecification = specification.getActionCards(actionCardRequest);
+
+		assertEquals(TOTAL_EMPIRE_ACTION_CARDS, actionCardRepository.findAll(actionCardSpecification).size());
 	}
 
 	@Test
 	void givenAllActionCardsAreAvailable_thenThirtyFourActionCardsAreReturned() {
-		assertEquals(TOTAL_ACTION_CARDS, ((List<ActionCard>)actionCardRepository.findAll()).size());
+		assertEquals(TOTAL_ACTION_CARDS, actionCardRepository.findAll().size());
 	}
 }

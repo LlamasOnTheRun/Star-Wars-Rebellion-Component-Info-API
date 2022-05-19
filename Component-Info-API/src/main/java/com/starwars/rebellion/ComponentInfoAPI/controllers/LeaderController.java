a349401598d37dd94abe22b2848bde64b3475110
Ryan Llamas <49294244+LlamasOnTheRun@users.noreply.github.com>
2022-05-19 18:29:25 -0500

    Deleting old endpoints
    Signed-off-by: Ryan Llamas <49294244+LlamasOnTheRun@users.noreply.github.com>

--- Component-Info-API/src/main/java/com/starwars/rebellion/ComponentInfoAPI/controllers/LeaderController.java
+++ Component-Info-API/src/main/java/com/starwars/rebellion/ComponentInfoAPI/controllers/LeaderController.java
@@ -1,18 +1,21 @@
 package com.starwars.rebellion.ComponentInfoAPI.controllers;
 
 import com.starwars.rebellion.ComponentInfoAPI.dao.entities.Leader;
-import com.starwars.rebellion.ComponentInfoAPI.dao.entities.embeddables.Faction;
 import com.starwars.rebellion.ComponentInfoAPI.dao.request.LeaderRequest;
 import com.starwars.rebellion.ComponentInfoAPI.repositories.LeaderRepository;
 import com.starwars.rebellion.ComponentInfoAPI.specification.LeaderSpecification;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
-import static com.starwars.rebellion.ComponentInfoAPI.utils.APIConstants.*;
+import static com.starwars.rebellion.ComponentInfoAPI.utils.APIConstants.BASE_CONTROLLER_PATH;
+import static com.starwars.rebellion.ComponentInfoAPI.utils.APIConstants.LEADER_ENDPOINT;
 
 @Controller
 @RequestMapping(value=BASE_CONTROLLER_PATH)
@@ -24,24 +27,6 @@ public class LeaderController {
 
     @Autowired
     LeaderSpecification leaderSpecification;
-
-    @GetMapping(path= GET_LEADER_ENDPOINT_EMPIRE)
-    @ResponseBody
-    public String getLeaderEmpire() {
-        return leaderRepository.findByName("Colonel Yularen").getName();
-    }
-
-    @GetMapping(path=GET_ALL_REBEL_LEADERS_ENDPOINT)
-    @ResponseBody
-    public List<Leader> getAllRebelLeaders() {
-        return leaderRepository.findAllByFaction(Faction.REBEL);
-    }
-
-    @GetMapping(path=GET_ALL_IMPERIAL_LEADERS_ENDPOINT)
-    @ResponseBody
-    public List<Leader> getAllEmpireLeaders() {
-        return leaderRepository.findAllByFaction(Faction.IMPERIAL);
-    }
 
     @PostMapping(path=LEADER_ENDPOINT)
     @ResponseBody

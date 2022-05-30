e0ed4d9773a889d5894eeaacd3b43fcb4a39f6ea
Ryan Llamas <49294244+LlamasOnTheRun@users.noreply.github.com>
2022-05-30 17:41:12 -0500

    Cleaned up repository methods
    Signed-off-by: Ryan Llamas <49294244+LlamasOnTheRun@users.noreply.github.com>

--- Component-Info-API/src/main/java/com/starwars/rebellion/ComponentInfoAPI/repositories/LeaderRepository.java
+++ Component-Info-API/src/main/java/com/starwars/rebellion/ComponentInfoAPI/repositories/LeaderRepository.java
@@ -1,7 +1,6 @@
 package com.starwars.rebellion.ComponentInfoAPI.repositories;
 
 import com.starwars.rebellion.ComponentInfoAPI.dao.entities.Leader;
-import com.starwars.rebellion.ComponentInfoAPI.dao.entities.embeddables.Faction;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
@@ -9,7 +8,5 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import java.util.List;
 
 public interface LeaderRepository extends JpaRepository<Leader, Integer>, JpaSpecificationExecutor<Leader> {
-    Leader findByName(String name);
-    List<Leader> findAllByFaction(Faction faction);
     List<Leader> findAll(Specification<Leader> spec);
 }

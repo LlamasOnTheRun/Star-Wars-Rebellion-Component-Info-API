baf9b0d9a02b4e188fb47edd18166783a6613ee2
Ryan Llamas <49294244+LlamasOnTheRun@users.noreply.github.com>
2022-06-06 19:32:12 -0500

    -Added additional sepcification tests
    Signed-off-by: Ryan Llamas <49294244+LlamasOnTheRun@users.noreply.github.com>

--- Component-Info-API/src/test/java/com/starwars/rebellion/ComponentInfoAPI/specification/RegionSpecificationTest.java
+++ Component-Info-API/src/test/java/com/starwars/rebellion/ComponentInfoAPI/specification/RegionSpecificationTest.java
@@ -1,9 +1,17 @@
 package com.starwars.rebellion.ComponentInfoAPI.specification;
 
+import com.starwars.rebellion.ComponentInfoAPI.dao.entities.Region;
+import com.starwars.rebellion.ComponentInfoAPI.dao.request.RegionRequest;
 import com.starwars.rebellion.ComponentInfoAPI.repositories.RegionRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static com.starwars.rebellion.ComponentInfoAPI.utils.APIConstants.REGION_ID_EXAMPLE;
+import static com.starwars.rebellion.ComponentInfoAPI.utils.APIConstants.TOTAL_REGIONS;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class RegionSpecificationTest {
@@ -16,36 +24,67 @@ public class RegionSpecificationTest {
 
     @Test
     void givenIDIsNull_thenPredicateIsNotAdded() {
+        RegionRequest regionRequest = new RegionRequest();
+        regionRequest.setId(null);
 
-    }
+        List<Region> regionList = regionRepository
+                .findAll(regionSpecification.getRegion(regionRequest));
 
-    @Test
-    void givenIDIsBlank_thenPredicateIsNotAdded() {
-
+        assertEquals(TOTAL_REGIONS, regionList.size());
     }
 
     @Test
     void givenIDIsProvided_thenPredicateIsAdded() {
+        RegionRequest regionRequest = new RegionRequest();
+        regionRequest.setId(REGION_ID_EXAMPLE);
 
+        List<Region> regionList = regionRepository
+                .findAll(regionSpecification.getRegion(regionRequest));
+
+        assertEquals(1, regionList.size());
     }
 
     @Test
     void givenSystemIsProvided_thenPredicateIsAdded() {
+        RegionRequest regionRequest = new RegionRequest();
+        regionRequest.setContainsSystem("Felucia");
 
+        List<Region> regionList = regionRepository
+                .findAll(regionSpecification.getRegion(regionRequest));
+
+        assertEquals(1, regionList.size());
     }
 
     @Test
     void givenSystemIsProvidedWithSimilarNaming_thenPredicateIsAdded() {
+        RegionRequest regionRequest = new RegionRequest();
+        regionRequest.setContainsSystem("fElUcIa");
 
+        List<Region> regionList = regionRepository
+                .findAll(regionSpecification.getRegion(regionRequest));
+
+        assertEquals(1, regionList.size());
     }
 
     @Test
     void givenSystemIsNull_thenPredicateIsNotAdded() {
+        RegionRequest regionRequest = new RegionRequest();
+        regionRequest.setContainsSystem(null);
 
+        List<Region> regionList = regionRepository
+                .findAll(regionSpecification.getRegion(regionRequest));
+
+        assertEquals(TOTAL_REGIONS, regionList.size());
     }
 
     @Test
     void givenSystemIsBlank_thenPredicateIsNotAdded() {
+        RegionRequest regionRequest = new RegionRequest();
+        regionRequest.setContainsSystem("");
 
+        List<Region> regionList = regionRepository
+                .findAll(regionSpecification.getRegion(regionRequest));
+
+        assertEquals(TOTAL_REGIONS, regionList.size());
     }
 }

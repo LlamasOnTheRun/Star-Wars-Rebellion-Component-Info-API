560db4bd4408e39c264c58cead9ee5b6abc41e2a
Ryan Llamas <49294244+LlamasOnTheRun@users.noreply.github.com>
2022-06-12 17:54:38 -0500

    -Added Specification Tests
    Signed-off-by: Ryan Llamas <49294244+LlamasOnTheRun@users.noreply.github.com>

--- Component-Info-API/src/test/java/com/starwars/rebellion/ComponentInfoAPI/repositories/SystemRepositoryTest.java
+++ Component-Info-API/src/test/java/com/starwars/rebellion/ComponentInfoAPI/repositories/SystemRepositoryTest.java
@@ -1,10 +1,15 @@
 package com.starwars.rebellion.ComponentInfoAPI.repositories;
 
+import com.starwars.rebellion.ComponentInfoAPI.dao.entities.System;
+import com.starwars.rebellion.ComponentInfoAPI.dao.request.SystemRequest;
+import com.starwars.rebellion.ComponentInfoAPI.specification.SystemSpecification;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static com.starwars.rebellion.ComponentInfoAPI.utils.APIConstants.TOTAL_SYSTEMS;
+import java.util.List;
+
+import static com.starwars.rebellion.ComponentInfoAPI.utils.APIConstants.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
@@ -13,8 +18,33 @@ public class SystemRepositoryTest {
     @Autowired
     SystemRepository systemRepository;
 
+    @Autowired
+    SystemSpecification systemSpecification;
+
     @Test
     void givenAllSystemDataIsAdded_thenThirtyTwoSystemsAreReturned() {
         assertEquals(TOTAL_SYSTEMS, systemRepository.findAll().stream().distinct().count());
+    }
+
+    @Test
+    void givenRequestHasRemoteFalse_thenProperDataIsReturned() {
+        SystemRequest systemRequest = new SystemRequest();
+        systemRequest.setRemote(false);
+
+        List<System> systemList = systemRepository
+                .findAll(systemSpecification.getSystem(systemRequest));
+
+        assertEquals(TOTAL_NON_REMOTE_SYSTEMS, systemList.size());
+    }
+
+    @Test
+    void givenRequestHasRemoteTrue_thenProperDataIsReturned() {
+        SystemRequest systemRequest = new SystemRequest();
+        systemRequest.setRemote(true);
+
+        List<System> systemList = systemRepository
+                .findAll(systemSpecification.getSystem(systemRequest));
+
+        assertEquals(TOTAL_REMOTE_SYSTEMS, systemList.size());
     }
 }

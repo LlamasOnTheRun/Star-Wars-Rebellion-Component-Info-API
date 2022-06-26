da67a5daa326b4c728486e8aa826a30a900154bc
ProfessorGundar <RyanDL1@hotmail.com>
2022-06-26 16:27:33 -0500

    -Added failing test case for level one objective cards

--- Component-Info-API/src/test/java/com/starwars/rebellion/ComponentInfoAPI/controllers/ObjectiveCardControllerTest.java
+++ Component-Info-API/src/test/java/com/starwars/rebellion/ComponentInfoAPI/controllers/ObjectiveCardControllerTest.java
@@ -30,7 +30,7 @@ class ObjectiveCardControllerTest {
 	}
 
 	@Test
-	void givenTitleProvided_thenReturnedDataIsAccurate() {
+	void givenTitleIsProvided_thenReturnedDataIsAccurate() {
 		ObjectiveCardRequest objectiveCardRequest = new ObjectiveCardRequest();
 		objectiveCardRequest.setTitle(INSPIRE_SYMPATHY_OBJECTIVE_CARD_TITLE);
 
@@ -38,5 +38,15 @@ class ObjectiveCardControllerTest {
 
 		assertEquals(1, objectiveCardList.size());
 		assertEquals(INSPIRE_SYMPATHY_OBJECTIVE_CARD_TITLE, objectiveCardList.get(0).getCardText().getTitle());
+	}
+
+	@Test
+	void givenDeckLevelOneIsProvided_thenReturnedDataIsAccurate() {
+		ObjectiveCardRequest objectiveCardRequest = new ObjectiveCardRequest();
+		objectiveCardRequest.setDeckLevelOne(true);
+
+		List<ObjectiveCard> objectiveCardList = objectiveCardController.getObjectiveCard(objectiveCardRequest);
+
+		assertEquals(0, objectiveCardList.size());
 	}
 }

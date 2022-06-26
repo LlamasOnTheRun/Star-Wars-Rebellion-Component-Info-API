8c782ad361b71c852bee648e8be76c5da6152c09
ProfessorGundar <RyanDL1@hotmail.com>
2022-06-26 17:45:57 -0500

    -Added more test cases for specification

--- Component-Info-API/src/test/java/com/starwars/rebellion/ComponentInfoAPI/specification/ObjectiveCardSpecificationTest.java
+++ Component-Info-API/src/test/java/com/starwars/rebellion/ComponentInfoAPI/specification/ObjectiveCardSpecificationTest.java
@@ -22,12 +22,103 @@ public class ObjectiveCardSpecificationTest {
     private ObjectiveCardRepository objectiveCardRepository;
 
     @Test
-    void givenIDIsLessThanOne_thenIDEqualPredicateIsNotAdded() {
+    void givenIDIsLessThanOne_thenPredicateIsNotAdded() {
         ObjectiveCardRequest objectiveCardRequest = new ObjectiveCardRequest();
         objectiveCardRequest.setId(0);
 
         List<ObjectiveCard> objectiveCardList = objectiveCardRepository.findAll(objectiveCardSpecification.getObjectiveCard(objectiveCardRequest));
 
         assertEquals(TOTAL_OBJECTIVE_CARDS, objectiveCardList.size());
+    }
+
+    @Test
+    void givenIDIsNull_thenPredicateIsNotAdded() {
+        ObjectiveCardRequest objectiveCardRequest = new ObjectiveCardRequest();
+        objectiveCardRequest.setId(null);
+
+        List<ObjectiveCard> objectiveCardList = objectiveCardRepository.findAll(objectiveCardSpecification.getObjectiveCard(objectiveCardRequest));
+
+        assertEquals(TOTAL_OBJECTIVE_CARDS, objectiveCardList.size());
+    }
+
+    @Test
+    void givenValidIDIsAdded_thenPredicateIsAdded() {
+        ObjectiveCardRequest objectiveCardRequest = new ObjectiveCardRequest();
+        objectiveCardRequest.setId(INSPIRE_SYMPATHY_OBJECTIVE_CARD_ID);
+
+        List<ObjectiveCard> objectiveCardList = objectiveCardRepository.findAll(objectiveCardSpecification.getObjectiveCard(objectiveCardRequest));
+
+        assertEquals(1, objectiveCardList.size());
+    }
+
+    @Test
+    void givenTitleIsNull_thenPredicateIsNotAdded() {
+        ObjectiveCardRequest objectiveCardRequest = new ObjectiveCardRequest();
+        objectiveCardRequest.setTitle(null);
+
+        List<ObjectiveCard> objectiveCardList = objectiveCardRepository.findAll(objectiveCardSpecification.getObjectiveCard(objectiveCardRequest));
+
+        assertEquals(TOTAL_OBJECTIVE_CARDS, objectiveCardList.size());
+    }
+
+    @Test
+    void givenTitleIsBlank_thenPredicateIsNotAdded() {
+        ObjectiveCardRequest objectiveCardRequest = new ObjectiveCardRequest();
+        objectiveCardRequest.setTitle("");
+
+        List<ObjectiveCard> objectiveCardList = objectiveCardRepository.findAll(objectiveCardSpecification.getObjectiveCard(objectiveCardRequest));
+
+        assertEquals(TOTAL_OBJECTIVE_CARDS, objectiveCardList.size());
+    }
+
+    @Test
+    void givenValidTitleIsAdded_thenPredicateIsAdded() {
+        ObjectiveCardRequest objectiveCardRequest = new ObjectiveCardRequest();
+        objectiveCardRequest.setTitle(INSPIRE_SYMPATHY_OBJECTIVE_CARD_TITLE);
+
+        List<ObjectiveCard> objectiveCardList = objectiveCardRepository.findAll(objectiveCardSpecification.getObjectiveCard(objectiveCardRequest));
+
+        assertEquals(1, objectiveCardList.size());
+    }
+
+    @Test
+    void givenSimilarTitleIsAdded_thenPredicateIsAdded() {
+        ObjectiveCardRequest objectiveCardRequest = new ObjectiveCardRequest();
+        objectiveCardRequest.setTitle("Inspire");
+
+        List<ObjectiveCard> objectiveCardList = objectiveCardRepository.findAll(objectiveCardSpecification.getObjectiveCard(objectiveCardRequest));
+
+        assertEquals(1, objectiveCardList.size());
+        assertEquals(INSPIRE_SYMPATHY_OBJECTIVE_CARD_TITLE, objectiveCardList.get(0).getCardText().getTitle());
+    }
+
+    @Test
+    void givenLevelOneIsNull_thenPredicateIsNotAdded() {
+        ObjectiveCardRequest objectiveCardRequest = new ObjectiveCardRequest();
+        objectiveCardRequest.setDeckLevelOne(null);
+
+        List<ObjectiveCard> objectiveCardList = objectiveCardRepository.findAll(objectiveCardSpecification.getObjectiveCard(objectiveCardRequest));
+
+        assertEquals(TOTAL_OBJECTIVE_CARDS, objectiveCardList.size());
+    }
+
+    @Test
+    void givenLevelOneIsFalse_thenPredicateIsNotAdded() {
+        ObjectiveCardRequest objectiveCardRequest = new ObjectiveCardRequest();
+        objectiveCardRequest.setDeckLevelOne(false);
+
+        List<ObjectiveCard> objectiveCardList = objectiveCardRepository.findAll(objectiveCardSpecification.getObjectiveCard(objectiveCardRequest));
+
+        assertEquals(TOTAL_OBJECTIVE_CARDS, objectiveCardList.size());
+    }
+
+    @Test
+    void givenLevelOneIsTrue_thenPredicateIsAdded() {
+        ObjectiveCardRequest objectiveCardRequest = new ObjectiveCardRequest();
+        objectiveCardRequest.setDeckLevelOne(true);
+
+        List<ObjectiveCard> objectiveCardList = objectiveCardRepository.findAll(objectiveCardSpecification.getObjectiveCard(objectiveCardRequest));
+
+        assertEquals(TOTAL_LEVEL_ONE_OBJECTIVE_CARDS, objectiveCardList.size());
     }
 }
